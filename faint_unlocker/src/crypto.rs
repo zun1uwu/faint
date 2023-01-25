533e35c9b9c80c028cecd1a10c991b6264274c2b
zun1 <zuni0@protonmail.com>
2023-01-25 14:26:37 +0100

    Optimized error handling

--- faint_unlocker/src/crypto.rs
+++ faint_unlocker/src/crypto.rs
@@ -18,7 +18,7 @@ pub fn decrypt(file: &path::PathBuf, customer: &Customer) {
             let cipher =
                 libaes::Cipher::new_256(customer.keypair.key.as_bytes().try_into().unwrap());
             let decrypted = cipher.cbc_decrypt(customer.keypair.iv.as_bytes(), &file_content);
-            if let Ok(_) = fs::write(file, decrypted) {
+            if fs::write(file, decrypted).is_ok() {
                 fs::rename(
                     file,
                     file.to_string_lossy()
